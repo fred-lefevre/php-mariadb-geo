b65f855e6a56074ce08ee7b3f7b44ae87efda07f
Frédéric LEFEVRE <fred.lefevre@gmail.com>
2023-05-08 19:24:10 +0200

    Affichage de la liste de tous les pays dans la page d'accueil

--- src/index.php
+++ src/index.php
@@ -1,5 +1,9 @@
 <?php
-
+$dbh = new PDO('mysql:host=127.0.0.1;dbname=geographie;port=3306;charset=utf8mb4', 'marco', 'polo');
+$stmt = $dbh->query('SELECT * FROM pays');
+$les_pays = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$nb_pays = count($les_pays);
+$dbh = null;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -16,9 +20,31 @@
             <h1>Accueil Base Géographie</h1>
         </div>
         <div class="container-fluid">
-
-
-
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="text-center" colspan="5"><?= $nb_pays ?> pays</th>
+                    </tr>
+                    <tr class="table-info">
+                        <th>code</th>
+                        <th>nom</th>
+                        <th>capitale</th>
+                        <th>population</th>
+                        <th>superficie</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($les_pays as $un_pays) { ?>
+                        <tr>
+                            <td><?= $un_pays["code"] ?></td>
+                            <td><?= $un_pays["nom"] ?></td>
+                            <td><?= $un_pays["capitale"] ?></td>
+                            <td><?= $un_pays["population"] ?></td>
+                            <td><?= $un_pays["superficie"] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
         <!-- Bootstrap Bundle with Popper -->
         <script src="./js/bootstrap.bundle.min.js"></script>
